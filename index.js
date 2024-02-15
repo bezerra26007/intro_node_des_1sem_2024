27a18e5d98505ad7bad25033ed5ceb0f67377b71
bezerra26007 <159494662+bezerra26007@users.noreply.github.com>
2024-02-15 20:41:11 -0300

    aula 15/02 2024

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log('Helo Word!');
+
+let nome = 'Gabriel';
+
+console.log('nome');
+
+
+
+
+
+let idade = 19;
+let altura = 1.90;
+
+console.log('Nome:' + nome + 'idade:' + idade + 'anos');
+console.log (`Nome:  ${nome} - Idade: ${idade} anos`);
+
+let trabalha = true;
+
+console.log(trabalha);
+console.log('trabalha:' + trabalha === true? 'sim': 'não' );
+
+if (trabalha === true) {
+    console.log('trabalha:sim');
+}else{
+    console.log('trabalha:não');
+}
+
+const sangue='b+';
+idade = 36;
+
+console.log(
+    'vNome:'+ typeof(nome) +'\n'
+    
+    
+)
+
+let endereco ={
+    "rua"
+}
+
